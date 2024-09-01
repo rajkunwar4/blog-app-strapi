5fb43836b38d4d3dbaaa65f2bee5707249934f53
rajkunwar4 <40rajkunwar@gmail.com>
2024-09-02 00:05:43 +0530

    config bug

--- next-blog/src/app/page.tsx
+++ next-blog/src/app/page.tsx
@@ -1,12 +1,27 @@
+"use client";
+
 import Blogs from "@/components/Blogs";
 import Categories from "@/components/Categories";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  const fetchCategories = async () => {
+    const categories = await fetch("http://localhost:1337/api/categories");
+    const res = await categories.json();
+    res.data.map((item: any) => {
+      console.log(item.attributes.Title);
+    });
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
   return (
     <div className="">
       <Categories />
-      <Blogs/>
+      <Blogs />
     </div>
   );
 }
